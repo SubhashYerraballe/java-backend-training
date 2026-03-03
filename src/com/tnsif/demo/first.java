35e630c56dc5d3aca64af6a0fc3d65be0fae41a8
sdvp7 <sdvp7@RAMUVEERA>
2026-03-03 15:08:09 +0530

    Initial commit - first Java project

---
+++ src/com/tnsif/demo/first.java
@@ -0,0 +1,11 @@
+package com.tnsif.demo;
+
+public class first {
+
+	public static void main(String[] args) {
+		System.out.println("Winter is coming");
+		// TODO Auto-generated method stub
+
+	}
+
+}
